b943b716b2d5d78cf4083a40b13976875a3a11c5
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-09-12 22:04:58 +0700

    home done, category 50%

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,7 +4,37 @@ import './Category.scss';
 export default function Category() {
     return (
         <div className="category-layout">
-            <h1>Category Page</h1>
+            <h2>Berbagai Pilihan Mobil Sesuai Kebutuhanmu</h2>
+            <div className="category-content">
+                <div className="category-wrapper">
+                    <img src={`../assets/steering-wheel-greyscale.png`} alt=""/>
+                    <h3>Sehari-hari</h3>
+                    <p>
+                        Berbagai macam membeli untuk keperluan harian, dapat lepas kunci!
+                    </p>
+                </div>
+                <div className="category-wrapper">
+                    <img src={`../assets/wedding-greyscale.png`} alt=""/>
+                    <h3>Pernikahan</h3>
+                    <p>
+                        Butuh mobil mewah untuk acara pernikahan mu? Kami sediakan berbagai pilihan!
+                    </p>
+                </div>
+                <div className="category-wrapper">
+                    <img src={`../assets/sun-umbrella-greyscale.png`} alt=""/>
+                    <h3>Liburan</h3>
+                    <p>
+                        Nyari mobil buat liburan rame-rame? Liburan ke puncak, Bogor, Bandung, Yuk!
+                    </p>
+                </div>
+                <div className="category-wrapper">
+                    <img src={`../assets/bus-greyscale.png`} alt=""/>
+                    <h3>Bus/Elf</h3>
+                    <p>
+                        Pergi berombongan? Bisa sewa bus/elf sekaligus supirnya!
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,14 @@ import './Home.scss';
 const Home = () => {
     return (
         <div className="home-layout">
+            <div className="content">
+                <h1>Cari Sewa Mobil?</h1>
+                <p>
+                    Mobil lepas kunci, mobil dengan supir, mobil
+                    <br /> untuk liburan puncak dan Bandung <br />
+                    semua Tap Rent Car solusinya!
+                </p>
+            </div>
         </div>
     )
 }
