afdbaa77ec97eeb8ae378230bc96c61f16fb67f8
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-09-15 15:09:40 +0700

    add gallery section

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,7 +4,15 @@ import './Gallery.scss'
 export default function Gallery() {
     return (
         <div className="gallery-layout">
-            <h1>Gallery Page</h1>
+            <h2>Galeri Mobil</h2>
+            <div className="content">
+                <img src={`../assets/galeri/1.jpeg`} alt=""/>
+                <img src={`../assets/galeri/2.jpeg`} alt=""/>
+                <img src={`../assets/galeri/3.jpeg`} alt=""/>
+                <img src={`../assets/galeri/4.jpeg`} alt=""/>
+                <img src={`../assets/galeri/5.jpeg`} alt=""/>
+                <img src={`../assets/galeri/6.jpeg`} alt=""/>
+            </div>
         </div>
     )
 }
