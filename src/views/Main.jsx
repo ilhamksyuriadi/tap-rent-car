5a41117c59e239c218988a50f9a3c269230c35da
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-09-16 09:35:09 +0700

    removing footer for temporary

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -3,7 +3,7 @@ import Home from '../components/Home';
 import Category from '../components/Category';
 import CarList from '../components/CarList';
 import Gallery from '../components/Gallery';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 
 const Main = () => {
     return(
