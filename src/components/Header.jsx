3069d8e59c97b7a2cc1741e1011c279101df2d24
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-09-19 07:27:49 +0700

    done, but need to be clean the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 import Logo from '../assets/logo/taprentcar_logo.png';
 import { Link, animateScroll } from 'react-scroll';
 
 export default function NavBar() {
+    const [state, setState] = useState({
+        toggle: false
+    })
+
     const scrollToTop = () => {
         animateScroll.scrollToTop();
+        // if (!state.toggle) {
+            handleLinkClick()
+        // }
+    }
+
+    const handleToggleClick = () => {
+        // let width = window.screen.width;
+        let menu = document.getElementById("navigation")
+        // console.log(typeof width)
+        if (state.toggle) {
+            setState({toggle: false})
+            menu.style.left = "100%"
+        } else {
+            setState({toggle: true})
+            menu.style.left = "0%"
+        }
+    }
+
+    const handleLinkClick = () => {
+        // let width = window.screen.width;
+        let menu = document.getElementById("navigation")
+        setState({toggle: false})
+        menu.style.left = "100%"
     }
 
     return (
         <header>
-            <img src={Logo} alt="logo" onClick={scrollToTop}/>
-            <nav>
+            <img className="logo" src={Logo} alt="logo" onClick={scrollToTop}/>
+            <nav id="navigation">
                 <ul>
                     {/* <li><a href="#home">Home</a></li> */}
                     <li>
@@ -22,6 +49,7 @@ export default function NavBar() {
                             smooth={true}
                             duration={1000}
                             offset={-75}
+                            onClick={handleLinkClick}
                         >
                         Home
                         </Link>
@@ -34,6 +62,7 @@ export default function NavBar() {
                             smooth={true}
                             duration={1000}
                             offset={-75}
+                            onClick={handleLinkClick}
                         >
                         Kategori
                         </Link>
@@ -46,6 +75,7 @@ export default function NavBar() {
                             smooth={true}
                             duration={1000}
                             offset={-75}
+                            onClick={handleLinkClick}
                         >
                         Harga
                         </Link>
@@ -58,12 +88,23 @@ export default function NavBar() {
                             smooth={true}
                             duration={1000}
                             offset={-75}
+                            onClick={handleLinkClick}
                         >
                         Galeri
                         </Link>
                     </li>
                 </ul>
             </nav>
+            <span className="hamburger">
+                {
+                    state.toggle 
+                    ? <img onClick={handleToggleClick} className="hamburger-logo" src={'../assets/logo/close.png'} alt=""/>
+                    : <img onClick={handleToggleClick} className="hamburger-logo" src={'../assets/logo/square.png'} alt=""/>
+                }
+                {/* <img className="hamburger-logo" src={'../assets/logo/square.png'} alt=""/> */}
+                {/* <img className="hamburger-logo" src={'../assets/logo/close.png'} alt=""/> */}
+                {/* <p className="hamburger-close">&#10006;</p> */}
+            </span>
         </header>
     )
 }
